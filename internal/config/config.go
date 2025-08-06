dc5efb8e91d2c0d21e7828b0bffc0f3419f18a04
Sergey Kharchenko <skharchenko87@yandex.ru>
2025-08-06 23:45:32 +0300

    feat: загрузка конфига

---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type Config struct {
+	Source string `yaml:"source"`
+	Log    Log    `yaml:"log"`
+	// ToDo
+}
+
+type Log struct {
+	Level  string `yaml:"level,omitempty"`
+	Format string `yaml:"format,omitempty"`
+}
+
+func LoadConfig(filePath string) (Config, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, fmt.Errorf("Error reading config file", filePath, err)
+	}
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return Config{}, fmt.Errorf("Error parsing config file", filePath, err)
+	}
+
+	return cfg, nil
+}
