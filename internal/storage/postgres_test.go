c326dd99f22d153ab7a702160583ead3c9a13ba5
Sergey Kharchenko <skharchenko87@yandex.ru>
2025-08-21 19:06:08 +0300

    style: корректировка строки для соблюдения форматирования

--- internal/storage/postgres_test.go
+++ internal/storage/postgres_test.go
@@ -11,7 +11,8 @@ func Test_newInsertProductQuery(t *testing.T) {
 		args args
 		want string
 	}{
-		{"TestCase 1", args{4}, "INSERT INTO product(name, protein, fat, carbohydrate, kcal) VALUES ($1,$2,$3,$4,$5), ($6,$7,$8,$9,$10), ($11,$12,$13,$14,$15), ($16,$17,$18,$19,$20)"},
+		{"TestCase 1", args{4}, `INSERT INTO product(name, protein, fat, carbohydrate, kcal) 
+					VALUES ($1,$2,$3,$4,$5), ($6,$7,$8,$9,$10), ($11,$12,$13,$14,$15), ($16,$17,$18,$19,$20)`},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
