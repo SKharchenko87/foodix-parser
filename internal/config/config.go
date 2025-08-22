e30682a09be0fe3f0ee955fb1b90e85b0b9ef08d
Sergey Kharchenko <skharchenko87@yandex.ru>
2025-08-22 12:02:06 +0300

    style: корректировка текстов ошибок

--- internal/config/config.go
+++ internal/config/config.go
@@ -34,12 +34,12 @@ type Log struct {
 func LoadConfig(filePath string) (Config, error) {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
-		return Config{}, fmt.Errorf("Error reading config file %s %v", filePath, err)
+		return Config{}, fmt.Errorf("error reading config file %s %v", filePath, err)
 	}
 	var cfg Config
 	err = yaml.Unmarshal(data, &cfg)
 	if err != nil {
-		return Config{}, fmt.Errorf("Error parsing config file %s %v", filePath, err)
+		return Config{}, fmt.Errorf("error parsing config file %s %v", filePath, err)
 	}
 
 	return cfg, nil
